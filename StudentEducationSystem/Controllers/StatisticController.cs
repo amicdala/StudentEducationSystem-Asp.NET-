398790d1b6244bac12d4b2aabffe894f5a708a54
enesoral <enesoral.seng@gmail.com>
2019-12-26 13:56:41 +0300

    bugs fixed and examcontroller refactored

--- StudentEducationSystem/Controllers/StatisticController.cs
+++ StudentEducationSystem/Controllers/StatisticController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StudentEducationSystem.Models;
 
 namespace StudentEducationSystem.Controllers
 {
     public class StatisticController : Controller
     {
-        // GET: Statistic
+        private readonly EducationSystemContext _context = new EducationSystemContext();
         public ActionResult Index()
         {
-            return View();
+            int studentId = Convert.ToInt32(Session["StudentId"]);
+            List<Exam> exams = GetLastThreeExamByStudentId(studentId);
+            
+            return View(exams[0]);
+        }
+
+        private List<Exam> GetLastThreeExamByStudentId(int studentId)
+        {
+            return _context.Exams.Where(x => x.StudentId == studentId).OrderByDescending(x => x.Date).Take(3).ToList();
         }
     }
 }
