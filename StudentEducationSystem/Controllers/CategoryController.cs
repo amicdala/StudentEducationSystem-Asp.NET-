365d38e0ce11612ad266a8f0b09fe78d313c4ee0
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2020-01-01 01:16:44 +0300

    Question create form edited and category create feature added.

---
+++ StudentEducationSystem/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using StudentEducationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentEducationSystem.Controllers
+{
+    [Authorize(Roles = "T")]
+    public class CategoryController : Controller
+    {
+        EducationSystemContext context = new EducationSystemContext();
+        // GET: Category
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormCollection form)
+        {
+            string name = form["name"];
+            int teacherId = GetTeacherID();
+            Category category = new Category() { Name = name, TeacherId = teacherId };
+            context.Categories.Add(category);
+            context.SaveChanges();
+            ToastrService.AddToUserQueue(new Toastr("Kategori Eklendi", null, ToastrType.Success));
+            return RedirectToAction("Create", "Question");
+        }
+
+        private int GetTeacherID()
+        {
+            return Convert.ToInt32(Session["TeacherId"]);
+        }
+    }
+}
